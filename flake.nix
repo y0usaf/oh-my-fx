9cac53733ea524b08110cdfa0fab17511abd4ba3
y0usaf <OA99@Outlook.com>
2026-08-19 23:26:27 -0400

    Keep Nix flake package-only

--- flake.nix
+++ flake.nix
@@ -1,23 +1,28 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
 
-  outputs = { nixpkgs, ... }:
-    let
-      systems = [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
-      eachSystem = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
-    in
-    {
-      packages = eachSystem (pkgs: {
-        default = pkgs.stdenv.mkDerivation {
-          name = "fx";
-          src = ./.;
-          nativeBuildInputs = [ pkgs.makeBinaryWrapper pkgs.zig_0_16 ];
-          postFixup = "wrapProgram $out/bin/fx --set FX_AUTO_UPGRADE 0";
-          meta.mainProgram = "fx";
-        };
-      });
-      devShells = eachSystem (pkgs: {
-        default = pkgs.mkShell { packages = [ pkgs.zig_0_16 ]; };
-      });
-    };
+  outputs = { nixpkgs, ... }: {
+    packages = nixpkgs.lib.genAttrs
+      [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ]
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.stdenv.mkDerivation {
+            name = "fx";
+            src = ./.;
+            nativeBuildInputs = [ pkgs.makeBinaryWrapper pkgs.zig ];
+            postInstall = ''
+              install -Dm444 LICENSE "$out/share/licenses/fx/LICENSE"
+              install -Dm444 THIRD_PARTY_NOTICES.md "$out/share/licenses/fx/THIRD_PARTY_NOTICES.md"
+            '';
+            postFixup = "wrapProgram $out/bin/fx --set FX_AUTO_UPGRADE 0";
+            meta = {
+              license = pkgs.lib.licenses.asl20;
+              mainProgram = "fx";
+            };
+          };
+        });
+  };
 }
